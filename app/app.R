7bb45c72e07d91c17756f82a037163fa8d83dca7
Teccch <enesalweswasi@gmail.com>
2020-11-29 01:00:38 +0100

    RStudio

--- app/app.R
+++ app/app.R
@@ -6,7 +6,11 @@ library(tidyr)
 library(DT)
 library(ggplot2)
 
+# Ggplot theme and palette ----
 theme_set(theme_bw())
+
+custom.col <- c("#999999", "#E69F00", "#56B4E9", "#009E73",
+                "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
 # Data ----
 load(file = "datasets.rda")
@@ -14,11 +18,11 @@ load(file = "datasets.rda")
 # UI ----
 ui <- fluidPage(
   theme = shinytheme("lumen"),
-  navbarPage("The Uneven Crime Drop"),
+  navbarPage("The Uneven Crime drop"),
   sidebarLayout(
     sidebarPanel(
       width = 3,
-      helpText("Create graph and table from crime data"),
+      helpText("Create graph and table from crime data gathered trough The Uneven Crime Drop project (Department of Criminology, University of Stockholm)"),
       
       # Drop-down list ----
       selectInput("var", 
@@ -26,7 +30,7 @@ ui <- fluidPage(
                   choices = list(" " = c(
                     "Polisanmälda brott per 100 000 invånare",
                     "Antal lagföringar samtliga brott",
-                    "Antal lagföringar respektive antal lagförda brott per"),
+                    "Antal lagföringar respektive antal lagförda brott per 100 000 invånare"),
                     "Lagföringar:" = c(
                       "Våld-, stöld- respektive narkotikabrott",
                       "Samtliga brott, efter kön",
@@ -60,39 +64,62 @@ ui <- fluidPage(
       
       # Slider ----
       sliderInput("range", strong("Year"), 
-                  1975, 2017, c(1975,2017), sep= "", step = 1),
+                  1973, 2017, c(1973,2017), sep= "", step = 1),
       
       # Checkbox crime type ----
-      checkboxGroupInput("checkcrime", strong("Crime"), 
-                         choices = unique(one_one_l$Crime),
-                         selected = one_one_l$Crime[1:4]),
+      conditionalPanel(
+        condition = 
+          "input.var == 'Polisanmälda brott per 100 000 invånare' || 
+        input.var == 'Antal lagföringar samtliga brott'",
+        checkboxGroupInput("checkcrime", strong("Crime"), 
+                           choices = unique(one_one_l$Crime),
+                           selected = one_one_l$Crime[1:4], inline = TRUE)
+      ),
       
       # Checkbox age-group ----
       checkboxGroupInput("checkage", strong("Age"), 
-                         choices = unique(three_five_l$Age),
-                         selected = three_five_l$Age[1:13]),
+                         choices = unique(three_three_four_l$Age),
+                         selected = three_three_four_l$Age[1:13]),
+      # Checkbox region ----
+      checkboxGroupInput("checkregion", strong("Härkomst"), 
+                         choices = c("Sverige", "Sverige, utlandsfödda föräldrar", "Västländer", "Övriga länder"),
+                         selected = c("Sverige", "Sverige, utlandsfödda föräldrar", "Västländer", "Övriga länder")),
       tags$br(),
       
       # Download buttons ----
-      downloadButton("downloadPlot", "Download Plot"),
-      downloadButton('downloadData', 'Download Table')
+      downloadButton("downloadPlot", "Download graph"),
+      downloadButton('downloadData', 'Download table'),
+      
+      tags$br(),
+      tags$br(),
+      tags$a(
+        href="https://github.com/weswasi/UnevenCrimeDrop", 
+        tags$img(src="git.png",
+                 width="40",
+                 height="35")
+      ),
+      tags$a(href="https://www.su.se/profiles/enal3400-1.424017", "Enes Al Weswasi")
+      
     ),
     
     mainPanel(
       tabsetPanel(type="tabs",
                   
-                  #Tab 1 Plots ----
-                  tabPanel(title="Plot",
+                  # Tab 1 Plots ----
+                  tabPanel(title = "Graph",
                            tags$br(),
-                           column(10, 
-                                  title = "Reported crime",
-                                  plotOutput("plot", height = 500))),
+                           column(10, plotOutput("plot", width  = 1000, height = 700))),
                   
-                  #Tab 2 Datatable ----
-                  tabPanel(title="Table",
+                  # Tab 2 Datatable ----
+                  tabPanel(title = "Table",
                            tags$br(),
                            column(7, dataTableOutput("mytable"))
-                  )
+                  ),
+                  
+                  #Tab 3 PDF ---
+                  tabPanel(title = "Description of graph and table from the report (in Swedish)",
+                           tags$br(),
+                           uiOutput("pdfview"))
       )
     )
   )
@@ -104,60 +131,278 @@ server <- function(input, output) {
   # Create plot ----
   plotInput <- reactive({
     
-    # Force user input atleast one value ----
+    # Force user input at least one Value ----
     validate(
       need(input$checkcrime, "Please select a crime :)"),
       need(input$checkage, "Please select age :)")
     )
     
+    # Plot ---
     if (input$var == "Polisanmälda brott per 100 000 invånare") {
-      one_one_l <- one_one_l %>% 
-        subset(Year >= input$range[1] & Year <= input$range[2] & Crime %in% input$checkcrime)
-      
-      ggplot(one_one_l, aes(Year, Value, color = Crime)) +
+      one_one_l %>% 
+        subset(Year >= input$range[1] & Year <= input$range[2] & Crime %in% input$checkcrime) %>% 
+        ggplot(aes(Year, Value, color = Crime)) +
         geom_line(size = 1.2) +
-        geom_point(size = 3) +
-        labs(color = "Crime",
-             y = "Crimes per 100 000",
-             x = "Year") +
+        geom_point(size = 2) +
+        labs(title = "Polisanmälda brott per 100 000 invånare",
+             y = "Crime per 100 000",
+             x = "Year",
+             color = "Crime") +
         scale_x_continuous(breaks = seq(1975, 2017, by = 3)) +
-        theme(text = element_text(size=15), 
-              axis.text.x = element_text(angle = -45))
+        theme(plot.title = element_text(color="gray50"),
+              text = element_text(size=15), 
+              axis.text.x = element_text(angle = -45)) +
+        scale_colour_manual(values=custom.col)
     }
     
-    else{
-      
-      if (input$var == "Samtliga brott, efter kön") {
-        three_five_l <- three_five_l %>% 
-          subset(Year >= input$range[1] & Year <= input$range[2] & Age %in% input$checkage)
-        
-        ggplot(three_five_l, aes(Year, Value, color = Age)) +
-          facet_grid(Gender ~ ., scales="free") +
-          geom_line(size = 1.2) +
-          geom_point(size = 3) +
-          labs(color = "Age",
-               y = "Crimes ",
-               x = "Year") +
-          scale_x_continuous(breaks = seq(1975, 2017, by = 3)) +
-          theme(text = element_text(size=15), 
-                axis.text.x = element_text(angle = -45))
-      }
+    else if (input$var == "Antal lagföringar samtliga brott") {
+      one_two_l %>% 
+        subset(Year >= input$range[1] & Year <= input$range[2]) %>% 
+        ggplot(aes(Year, Value, color = Source)) +
+        geom_line(size = 1.2) +
+        geom_point(size = 2) +
+        labs(title = "Antal lagföringar samtliga brott",
+             y = "Crime ",
+             x = "Year",
+             color = "Data source") +
+        scale_x_continuous(breaks = seq(1975, 2017, by = 3)) +
+        theme(
+          plot.title = element_text(color="gray50"),
+          text = element_text(size=15),
+          axis.text.x = element_text(angle = -45)) +
+        scale_colour_manual(values=custom.col)
     }
+    
+    else if (input$var == "Antal lagföringar respektive antal lagförda brott per 100 000 invånare"){
+      one_three_l %>%
+        subset(Year >= input$range[1] & Year <= input$range[2]) %>% 
+        ggplot(aes(Year, Value, color = Prosecutions)) +
+        geom_line(size = 1.2) +
+        geom_point(size = 2) +
+        labs(title = "Antal lagföringar respektive antal lagförda brott per 100 000 invånare",
+             y = "Prosecutions per 100 000",
+             x = "Year",
+             color = "Prosecutions") +
+        scale_x_continuous(breaks = seq(1973, 2017, by = 4)) +
+        theme(
+          plot.title = element_text(color="gray50"),
+          text = element_text(size=15),
+          axis.text.x = element_text(angle = -45)) +
+        scale_colour_manual(values=custom.col)
+    }
+    
+    else if (input$var == "Våld-, stöld- respektive narkotikabrott"){
+      one_four_l %>%
+        subset(Year >= input$range[1] & Year <= input$range[2] & Crime %in% input$checkcrime) %>% 
+        ggplot(aes(Year, Value, color = Crime)) +
+        geom_line(size = 1.2) +
+        geom_point(size = 2) +
+        labs(title = "Våld-, stöld- respektive narkotikabrott",
+             y = "Prosecutions per 100 000",
+             x = "Year",
+             color = "Crime") +
+        scale_x_continuous(breaks = seq(1973, 2017, by = 4)) +
+        theme(
+          plot.title = element_text(color="gray50"),
+          text = element_text(size=15),
+          axis.text.x = element_text(angle = -45)) +
+        scale_colour_manual(values=custom.col)
+    }
+    
+    else if (input$var == "Samtliga brott, efter kön") {
+      three_one_l %>%
+        subset(Year >= input$range[1] & Year <= input$range[2]) %>% 
+        ggplot(aes(Year, Value, color = Gender)) +
+        geom_line(size = 1.2) +
+        geom_point(size = 2) +
+        labs(title = "Samtliga brott, efter kön",
+             y = "Prosecutions per 100 000",
+             x = "Year",
+             color = "Gender") +
+        scale_x_continuous(breaks = seq(1973, 2017, by = 4)) +
+        theme(
+          plot.title = element_text(color="gray50"),
+          text = element_text(size=15),
+          axis.text.x = element_text(angle = -45)) +
+        scale_colour_manual(values=custom.col)
+      
+    }
+    
+    else if (input$var == "Våld-, stöld-, respektive narkotikabrott efter kön") {
+      three_two_l %>%
+        subset(Year >= input$range[1] & Year <= input$range[2] & Crime %in% input$checkcrime) %>% 
+        ggplot(aes(Year, Value, color = Crime)) +
+        facet_grid(Gender ~ ., scales="free") +
+        geom_line(size = 1.2) +
+        geom_point(size = 2) +
+        labs(title = "Våld-, stöld-, respektive narkotikabrott efter kön",
+             y = "Prosecutions per 100 000",
+             x = "Year",
+             color = "Crime") +
+        scale_x_continuous(breaks = seq(1973, 2017, by = 4)) +
+        theme(
+          plot.title = element_text(color="gray50"),
+          text = element_text(size=15),
+          axis.text.x = element_text(angle = -45)) +
+        scale_colour_manual(values=custom.col)
+      
+    }
+    
+    else if (input$var == "Samtliga brott efter ålder och kön") {
+      three_three_four_l %>% 
+        subset(Year >= input$range[1] & Year <= input$range[2] & Age %in% input$checkage) %>% 
+        ggplot(aes(Year, Value, color = Age)) +
+        facet_grid(Gender ~ ., scales="free") +
+        geom_line(size = 1.2) +
+        geom_point(size = 2) +
+        labs(title = "Samtliga brott, efter kön",
+             y = "Crime ",
+             x = "Year",
+             color = "Age") +
+        scale_x_continuous(breaks = seq(1973, 2017, by = 4)) +
+        theme(
+          plot.title = element_text(color="gray50"),
+          text = element_text(size=15), 
+          axis.text.x = element_text(angle = -45)) +
+        scale_colour_manual(values=custom.col)
+    }
+    
+    else if (input$var == "Stöldbrott efter ålder och kön") {
+      three_five_ad_l %>%
+        subset(Year >= input$range[1] & Year <= input$range[2] & Age %in% input$checkage) %>% 
+        ggplot(aes(Year, Value, color = Age)) +
+        facet_grid(Gender ~ ., scales="free") +
+        geom_line(size = 1.2) +
+        geom_point(size = 2) +
+        labs(title = "Stöldbrott efter ålder och kön",
+             y = "Prosecutions per 100 000",
+             x = "Year",
+             color = "Age") +
+        scale_x_continuous(breaks = seq(1973, 2017, by = 4)) +
+        theme(
+          plot.title = element_text(color="gray50"),
+          text = element_text(size=15),
+          axis.text.x = element_text(angle = -45)) +
+        scale_colour_manual(values=custom.col)
+      
+    }
+    
+    else if (input$var == "Våldsbrott efter ålder och kön") {
+      three_five_be_l %>%
+        subset(Year >= input$range[1] & Year <= input$range[2] & Age %in% input$checkage) %>% 
+        ggplot(aes(Year, Value, color = Age)) +
+        facet_grid(Gender ~ ., scales="free") +
+        geom_line(size = 1.2) +
+        geom_point(size = 2) +
+        labs(title = "Våldsbrott efter ålder och kön",
+             y = "Prosecutions per 100 000",
+             x = "Year",
+             color = "Age") +
+        scale_x_continuous(breaks = seq(1973, 2017, by = 4)) +
+        theme(
+          plot.title = element_text(color="gray50"),
+          text = element_text(size=15),
+          axis.text.x = element_text(angle = -45)) +
+        scale_colour_manual(values=custom.col)
+      
+    }
+    
+    else if (input$var == "Narkotikabrott efter ålder och kön") {
+      three_five_cf_l %>%
+        subset(Year >= input$range[1] & Year <= input$range[2] & Age %in% input$checkage) %>% 
+        ggplot(aes(Year, Value, color = Age)) +
+        facet_grid(Gender ~ ., scales="free") +
+        geom_line(size = 1.2) +
+        geom_point(size = 2) +
+        labs(title = "Narkotikabrott efter ålder och kön",
+             y = "Prosecutions per 100 000",
+             x = "Year",
+             color = "Age") +
+        scale_x_continuous(breaks = seq(1973, 2017, by = 4)) +
+        theme(
+          plot.title = element_text(color="gray50"),
+          text = element_text(size=15),
+          axis.text.x = element_text(angle = -45)) +
+        scale_colour_manual(values=custom.col)
+      
+    }
+    
   })
   
   # Create data for datatable ----
   tableInput <- reactive({
     
     if (input$var == "Polisanmälda brott per 100 000 invånare") {
-      one_one_w <- one_one_w %>% select(Year, input$checkcrime) %>% 
+      one_one_w <- one_one_w %>% 
+        select(Year, input$checkcrime) %>% 
         subset(Year >= input$range[1] & Year <= input$range[2])
     }
     
-    else{
-      if (input$var == "Samtliga brott, efter kön") {
-        three_five_w <- three_five_w %>% 
-          subset(Year >= input$range[1] & Year <= input$range[2])
-      }}
+    else if (input$var == "Antal lagföringar samtliga brott") {
+      one_two_w <- one_two_w %>% 
+        subset(Year >= input$range[1] & Year <= input$range[2])
+    }
+    
+    else if (input$var == "Antal lagföringar respektive antal lagförda brott per 100 000 invånare") {
+      one_three_w <- one_three_w %>% 
+        subset(Year >= input$range[1] & Year <= input$range[2])
+    }
+    
+    else if (input$var == "Våld-, stöld- respektive narkotikabrott") {
+      one_four_w <- one_four_w %>% 
+        subset(Year >= input$range[1] & Year <= input$range[2])
+    }
+    
+    else if (input$var == "Samtliga brott, efter kön") {
+      three_one_w <- three_one_w %>% 
+        subset(Year >= input$range[1] & Year <= input$range[2])
+    }
+    
+    else if (input$var == "Våld-, stöld-, respektive narkotikabrott efter kön") {
+      three_two_w <- three_two_w %>% 
+        subset(Year >= input$range[1] & Year <= input$range[2])
+    }
+    
+    else if (input$var == "Samtliga brott efter ålder och kön") {
+      three_three_four_w <- three_three_four_w %>% 
+        subset(Year >= input$range[1] & Year <= input$range[2]) %>% 
+        select(Year, ends_with(input$checkage))
+    }
+    
+    else if (input$var == "Stöldbrott efter ålder och kön") {
+      three_five_ad_w <- three_five_ad_w %>% 
+        subset(Year >= input$range[1] & Year <= input$range[2]) %>% 
+        select(Year, ends_with(input$checkage))
+    }
+    
+    else if (input$var == "Våldsbrott efter ålder och kön") {
+      three_five_be_w <- three_five_be_w %>% 
+        subset(Year >= input$range[1] & Year <= input$range[2]) %>% 
+        select(Year, ends_with(input$checkage))
+    }
+    
+    else if (input$var == "Narkotikabrott efter ålder och kön") {
+      three_five_cf_w <- three_five_cf_w %>% 
+        subset(Year >= input$range[1] & Year <= input$range[2]) %>% 
+        select(Year, ends_with(input$checkage))
+    }
+    
+  })
+  
+  # Reactive page number
+  output$pdfview <- renderUI({
+    
+    if (input$var == "Polisanmälda brott per 100 000 invånare") {
+      tags$iframe(style="height:780px; width:100%", src="rapport.pdf#page=4")
+    }
+    
+    else if (input$var == "Antal lagföringar samtliga brott") {
+      tags$iframe(style="height:980px; width:100%", src="rapport.pdf#page=30")
+    }
+    
+    else if (input$var == "Antal lagföringar respektive antal lagförda brott per 100 000 invånare") {
+      tags$iframe(style="height:980px; width:100%", src="rapport.pdf#page=12")
+    }
   })
   
   # Plot output ----
@@ -176,7 +421,8 @@ server <- function(input, output) {
           ordering = FALSE,
           dom = "t",
           pageLength = -1,
-          columnDefs = list(list(className = 'dt-center', targets = "_all"))))
+          columnDefs = list(list(className = 'dt-center', targets = "_all"))),
+        caption = htmltools::tags$caption(style = 'caption-side: top; color:gray; font-size:130% ;',input$var) )
   })
   
   # Download button content ----
--- datasets/datasets.R
+++ datasets/datasets.R
@@ -1,25 +1,131 @@
 library(dplyr)
 library(tidyr)
-library(tibble)
 library(readxl)
 
 # 1.1 ----
 # Wide
 one_one_w <- read_xlsx("./datasets/one_one.xlsx")
-one_one_w <- as_tibble(one_one_w)
 
-one_one_l <- pivot_longer(one_one_w, 2:5, names_to = "Crime", values_to = "Value")
+one_one_l <- one_one_w %>% 
+  pivot_longer(!Year, names_to = "Crime", values_to = "Value")
+
+# 1.2 ----
+# Wide
+one_two_w <- read_xlsx("./datasets/one_two.xlsx")
+
+#Long
+one_two_l <- one_two_w %>% 
+  pivot_longer(!Year, names_to = "Source", values_to = "Value")
+
+# 1.3 ----
+# Wide
+one_three_w <- read_xlsx("./datasets/one_three.xlsx")
+
+#Long
+one_three_l <- one_three_w %>% 
+  pivot_longer(!Year, names_to = "Prosecutions", values_to = "Value")
+
+# 1.4
+# Wide
+one_four_w <- read_xlsx("./datasets/one_four.xlsx")
+
+# Long
+one_four_l <- one_four_w %>% 
+  pivot_longer(!Year, names_to = "Crime", values_to = "Value")
+
+#3.2 ----
+# Wide
+three_two_w <- read_xlsx("./datasets/three_two.xlsx")
+
+# Long
+three_two_l <- three_two_w %>% 
+  pivot_longer(!Year, names_to = "Crime", values_to = "Value") %>% 
+  mutate(Gender = Crime) %>% 
+  select(Year, Gender, Crime, Value)
+
+
+three_two_l$Gender <- gsub("-.*", "", three_two_l$Gender)  
+three_two_l$Crime <- gsub(".*-", "", three_two_l$Crime)
 
 # 3_3_3_4 ----
 # Wide
-three_five_w <- read_xlsx("./datasets/three_five.xlsx")
-three_five_w <- as_tibble(three_five_w) 
+three_three_four_w <- read_xlsx("./datasets/three_three_four.xlsx")
 
 # Long
-three_five_l <- pivot_longer(three_five_w, 2:15, names_to = "Age", values_to = "Value")
-three_five_l <- three_five_l %>% mutate(Gender = Age)
-three_five_l$Gender <- gsub("_.*", "", three_five_l$Gender)  
-three_five_l$Age <- gsub(".*_", "", three_five_l$Age)
+three_three_four_l <- three_three_four_w %>% 
+  pivot_longer(!Year, names_to = "Age", values_to = "Value") %>% 
+  mutate(Gender = Age) %>% 
+  select(Year, Gender, Age, Value)
+
+three_three_four_l$Gender <- gsub(" .*", "", three_three_four_l$Gender)  
+three_three_four_l$Age <- gsub(".* ", "", three_three_four_l$Age)
+
+# 3_1 ----
+# Wide
+three_one_w <- read_xlsx("./datasets/three_one.xlsx")
+
+# Long
+three_one_l <- three_one_w %>% 
+  pivot_longer(!Year, names_to = "Gender", values_to = "Value")
+
+# 3_5_ab
+# Wide
+three_five_ad_w <- read_xlsx("./datasets/three_five_ad.xlsx")
+
+# Long
+three_five_ad_l <- three_five_ad_w %>% 
+  pivot_longer(!Year, names_to = "Age", values_to = "Value") %>% 
+  mutate(Gender = Age) %>%
+  select(Year, Gender, Age, Value)
+
+three_five_ad_l$Gender <- gsub(" .*", "", three_five_ad_l$Gender)  
+three_five_ad_l$Age <- gsub(".* ", "", three_five_ad_l$Age)
+
+# 3_5_be
+# Wide
+three_five_be_w <- read_xlsx("./datasets/three_five_be.xlsx")
+
+# Long
+three_five_be_l <- three_five_be_w %>% 
+  pivot_longer(!Year, names_to = "Age", values_to = "Value") %>% 
+  mutate(Gender = Age) %>%
+  select(Year, Gender, Age, Value)
+
+three_five_be_l$Gender <- gsub(" .*", "", three_five_be_l$Gender)  
+three_five_be_l$Age <- gsub(".* ", "", three_five_be_l$Age)
+
+# 3_5_be
+# Wide
+three_five_cf_w <- read_xlsx("./datasets/three_five_cf.xlsx")
+
+# Long
+three_five_cf_l <- three_five_cf_w %>% 
+  pivot_longer(!Year, names_to = "Age", values_to = "Value") %>% 
+  mutate(Gender = Age) %>%
+  select(Year, Gender, Age, Value)
+
+three_five_cf_l$Gender <- gsub(" .*", "", three_five_cf_l$Gender)  
+three_five_cf_l$Age <- gsub(".* ", "", three_five_cf_l$Age)
 
 # Save ----
-save(one_one_w, one_one_l, three_five_w, three_five_l, file = "./app/datasets.rda")
+save(one_one_w, 
+     one_one_l, 
+     one_two_w,
+     one_two_l,
+     one_three_w,
+     one_three_l,
+     one_four_w,
+     one_four_l,
+     three_one_w,
+     three_one_l,
+     three_two_w,
+     three_two_l,
+     three_three_four_w, 
+     three_three_four_l, 
+     three_five_ad_w,
+     three_five_ad_l,
+     three_five_be_w,
+     three_five_be_l,
+     three_five_cf_w,
+     three_five_cf_l,
+     file = "./app/datasets.rda")
